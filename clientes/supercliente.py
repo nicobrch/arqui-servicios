dba2013e92a91ec7af174ed4fe222cc4adba6241
Diego27z <116527185+Diego27z@users.noreply.github.com>
2023-11-21 03:22:06 -0300

    Update supercliente.py

--- clientes/supercliente.py
+++ clientes/supercliente.py
@@ -11,12 +11,12 @@ def print_menu():
 
 def print_menu2():
     print("Seleccione un archivo para ejecutar:")
-    print("1. Servicio de manejo de comentarios")
-    print("2. Servicio de manejo de bloques de horarios")
+    print("1. Servicio de manejo de asignacion de bloques de horario")
+    print("2. Servicio de manejo de comentarios")
 
 def execute_file(file_name):
     try:
-        os.system(f"python {file_name}")
+        os.system(f"python3 {file_name}")
     except Exception as e:
         print(f"Error al ejecutar el archivo {file_name}: {e}")
 
@@ -52,8 +52,8 @@ if __name__ == "__main__":
             break
         elif choice.isdigit() and 1 <= int(choice) <= 8:
             file_name = [
+                "asign_block.py",
                 "client_comment_management.py",
-                "schedule_block.py"
             ][int(choice) - 1]
 # Si es necesario se podria de aqui mismo iniciar el servicio tambien
             execute_file(file_name)
